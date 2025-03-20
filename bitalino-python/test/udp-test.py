79a85454e959336a4154426fa3f94a9474c633ea
Jumpei Saito <saijun721.1@gmail.com>
2025-03-20 23:43:15 +0900

    ok udp-python

--- bitalino-python/test/udp-test.py
+++ bitalino-python/test/udp-test.py
@@ -8,14 +8,18 @@ UDP_PORT = 8000  # ESP32側の受信用ポート
 # UDPソケットを作成
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# 送信する各値（例として角度X=128, 角度Y=64, スピード=200）
-angleX = 128  # 0～255
-angleY = 60   # 0～255
-speed  = 20  # 0～255
+a = 0
+while True:
+    # 送信する各値（例として角度X=128, 角度Y=64, スピード=200）
+    a += 1
+    a = a % 255
+    angleX = a  # 0～255
+    angleY = 60   # 0～255
+    speed  = 20  # 0～255
 
-# 3バイト（B: unsigned char）としてパックする
-data = struct.pack("BBB", angleX, angleY, speed)
+    # 3バイト（B: unsigned char）としてパックする
+    data = struct.pack("BBB", angleX, angleY, speed)
 
-# UDPパケット送信
-sock.sendto(data, (ESP32_IP, UDP_PORT))
-print("Binary command sent:", (angleX, angleY, speed))
+    # UDPパケット送信
+    sock.sendto(data, (ESP32_IP, UDP_PORT))
+    print("Binary command sent:", (angleX, angleY, speed))
