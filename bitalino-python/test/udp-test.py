702287adf03a05a9f1cd030fb4c38aaed3b98f6d
Jumpei Saito <saijun721.1@gmail.com>
2025-03-21 01:16:37 +0900

    ok udp

--- bitalino-python/test/udp-test.py
+++ bitalino-python/test/udp-test.py
@@ -1,8 +1,10 @@
 import socket
 import struct
+import time
 
 # ESP32のIPアドレス（WiFi接続後のESP32のIPに合わせる）
-ESP32_IP = "192.168.0.23"
+ESP32_IP = "192.168.0.23" #jcom
+# ESP32_IP = "192.168.99.218" #oppo
 UDP_PORT = 8000  # ESP32側の受信用ポート
 
 # UDPソケットを作成
@@ -15,7 +17,7 @@ while True:
     a = a % 255
     angleX = a  # 0～255
     angleY = 60   # 0～255
-    speed  = 20  # 0～255
+    speed  = 150  # 0～255
 
     # 3バイト（B: unsigned char）としてパックする
     data = struct.pack("BBB", angleX, angleY, speed)
@@ -23,3 +25,6 @@ while True:
     # UDPパケット送信
     sock.sendto(data, (ESP32_IP, UDP_PORT))
     print("Binary command sent:", (angleX, angleY, speed))
+
+    # 1秒待つ
+    time.sleep(0.008)
